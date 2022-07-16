ed4b46f42f689ff6c840951ed92cbca9afcd3333
Hannu Kujanpää <hannuku.hk@gmail.com>
2022-07-16 17:13:52 +0300

    Add project files

---
+++ PrisonerProblem/Shuffler.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+public static class Shuffler
+{
+    /// <summary>
+    /// This shuffles given array of integers using the https://en.wikipedia.org/wiki/Fisher–Yates_shuffle.
+    /// </summary>
+    /// <param name="arr">Array of integers to shuffle.</param>
+    public static void Shuffle(this int[] arr)
+    {
+        for (int elementNum = arr.Length; elementNum > 0; elementNum--)
+        {
+            var rng = RandomNumberGenerator.GetInt32(0, elementNum);
+            (arr[elementNum - 1], arr[rng]) = (arr[rng], arr[elementNum - 1]);
+        }
+    }
+}
